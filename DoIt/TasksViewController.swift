31bace500e3903e3915cc6a1275ccd5184d4e79d
Marcus Jessnitz <alliance2000@Marcuss-iMac.local>
2018-01-02 00:28:52 -0700

    App done - moving to core data!

--- DoIt/TasksViewController.swift
+++ DoIt/TasksViewController.swift
@@ -13,6 +13,7 @@ class TasksViewController: UIViewController, UITableViewDataSource, UITableViewD
     @IBOutlet weak var tableView: UITableView!
     
     var tasks : [Task] = []
+    var selectedIndex = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -44,6 +45,12 @@ class TasksViewController: UIViewController, UITableViewDataSource, UITableViewD
         return cell
     }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        selectedIndex = indexPath.row
+        let task = tasks[indexPath.row]
+        performSegue(withIdentifier: "selectTaskSegue", sender: task)
+    }
+    
     
     func makeTasks() -> [Task] {
         let task1 = Task()
@@ -68,8 +75,16 @@ class TasksViewController: UIViewController, UITableViewDataSource, UITableViewD
     
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        let nextVC = segue.destination as! CreateTaskViewController
-        nextVC.previousVC = self
+        if segue.identifier == "addSegue" {
+            let nextVC = segue.destination as! CreateTaskViewController
+            nextVC.previousVC = self
+        }
+        if segue.identifier == "selectTaskSegue" {
+            let nextVC = segue.destination as! CompleteTaskViewController
+            nextVC.task = sender as! Task
+            nextVC.previousVC = self
+        }
+
     }
     
 }
