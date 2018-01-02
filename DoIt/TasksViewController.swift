9e99e5ed4aaf2ce4abc516af2d9885f7aa10e497
Marcus Jessnitz <alliance2000@Marcuss-iMac.local>
2018-01-01 23:53:18 -0700

    Adding tasks completed!

--- DoIt/TasksViewController.swift
+++ DoIt/TasksViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  TasksViewController.swift
 //  DoIt
 //
 //  Created by Marcus Jessnitz on 01.01.18.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class TasksViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet weak var tableView: UITableView!
     
@@ -66,5 +66,10 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         performSegue(withIdentifier: "addSegue", sender: nil)
     }
     
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let nextVC = segue.destination as! CreateTaskViewController
+        nextVC.previousVC = self
+    }
     
 }
