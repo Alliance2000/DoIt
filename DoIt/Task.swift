744f4801b0f0ee7e488a825a5c3d12bba595348d
Marcus Jessnitz <alliance2000@Marcuss-iMac.local>
2018-01-01 23:16:22 -0700

    1st View Controller set up

---
+++ DoIt/Task.swift
@@ -0,0 +1,14 @@
+//
+//  Task.swift
+//  DoIt
+//
+//  Created by Marcus Jessnitz on 01.01.18.
+//  Copyright © 2018 Marcus Jessnitz. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    var name = ""
+    var important = false
+}
